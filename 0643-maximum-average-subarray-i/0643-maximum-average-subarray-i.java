feccd9ccbaf36a2005455567485618b4c12c5789
mayank333-OOP <bhatimayank124@gmail.com>
2026-08-03 22:49:13 +0530

    Time: 3 ms (73.74%), Space: 69.7 MB (45.38%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+      int left = 0 ;
+      int sum = 0 ; 
+      int maxSum = Integer.MIN_VALUE ;
+
+
+      for(int right =0 ; right <nums.length ; right++){
+         sum += nums[right] ;
+         if(right - left + 1 == k){
+            if(sum > maxSum){
+                maxSum = sum ;
+            }
+            sum -= nums[left] ;
+            left++ ;
+           }
+        }
+        double avg  = (double) maxSum/k ;
+        return avg ;
+    
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
